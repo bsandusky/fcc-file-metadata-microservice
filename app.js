52011fb1411be0fcc99f61121ce640658ee0bc25
Brett <brett.sandusky@gmail.com>
2017-02-13 00:31:58 +0000

    add multer to parse file upload from form

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require("express")
+const multer = require("multer")
 const app = express()
 
 app.use(express.static('public'))
@@ -7,6 +8,11 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     res.render('index.html')
 })
+
+app.post('/upload', multer({dest: './uploads'}).single('file'), function (req, res, next) {
+    res.send({file: req.file})
+})
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.IP + ":" + process.env.PORT)
--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-    
-    document.getElementById("fileUpload").addEventListener("change", (event) => {
-        console.log(event.target.files[0])
-    })
-    
-})()
